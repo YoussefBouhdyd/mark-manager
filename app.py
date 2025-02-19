6c44fc0ffc1943c93f099607a45907af80a64f4a
YoussefBouhdyd <bouhdyd2004@gmail.com>
2025-02-19 20:16:20 +0100

    add remember me to prvent asking for user and password for each time .

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,flash,redirect
+from flask import Flask,render_template,request,make_response,flash,redirect,session,url_for
 
 app = Flask(__name__,static_url_path="/")
 app.secret_key = "1234"
@@ -7,18 +7,26 @@ app.secret_key = "1234"
 @app.route("/",methods=["POST","GET"])
 def index():
     if request.method == "GET":
+        if request.cookies.get("login"):
+            return redirect(url_for("calc"))
         return render_template("index.html")
     elif request.method == "POST":
         user_name = request.form.get("user-name")
         password = request.form.get("password")
+        remember_me = request.form.get("remember")
         if (user_name == "Youssef" and password == "A@youssef123"):
-            return render_template("calc.html")
+            response = make_response(redirect(url_for("calc")))
+            if remember_me == "on":
+                response.set_cookie("login",user_name)
+            return response
         else:
             flash("Incorrect password or username !","login-alert")
             return redirect("/")
 
-@app.route("/calc",methods=["POST"])
-def clac():
+@app.route("/calc",methods=["POST","GET"])
+def calc():
+    if request.method == "GET":
+        return render_template("calc.html")
     student_name = request.form.get("student-name")
     si = request.form.get("si")
     ro = request.form.get("ro")
